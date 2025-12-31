8e30e0cafdf4e7ba6559fdf5ac5017e1cfedde69
Jesse Stover <j.stover.r@gmail.com>
2025-12-31 11:48:36 -0600

    Unique secret file names

--- functions/FaaSrSecretWorkflow/01_python_secret.py
+++ functions/FaaSrSecretWorkflow/01_python_secret.py
@@ -8,13 +8,13 @@ from FaaSr_py.client.py_client_stubs import (
 def python_secret(folder_name: str):
     value = faasr_secret("TEST_SECRET")
 
-    with open("secret.txt", "w") as f:
+    with open("secret_python.txt", "w") as f:
         f.write(value)
 
     invocation_id = faasr_invocation_id()
 
     faasr_put_file(
-        local_file="secret.txt",
-        remote_file=f"{invocation_id}/secret.txt",
+        local_file="secret_python.txt",
+        remote_file=f"{invocation_id}/secret_python.txt",
         remote_folder=folder_name,
     )
--- functions/FaaSrSecretWorkflow/02_r_secret.r
+++ functions/FaaSrSecretWorkflow/02_r_secret.r
@@ -1,13 +1,13 @@
 r_secret <- function(folder_name) {
     value <- faasr_secret("TEST_SECRET")
 
-    cat(value, file = "secret.txt", sep = "")
+    cat(value, file = "secret_r.txt", sep = "")
 
     invocation_id <- faasr_invocation_id()
 
-    remote_file <- paste0(invocation_id, "/secret.txt")
+    remote_file <- paste0(invocation_id, "/secret_r.txt")
     faasr_put_file(
-        local_file = "secret.txt",
+        local_file = "secret_r.txt",
         remote_file = remote_file,
         remote_folder = folder_name
     )
